08886e585713b88c2891efb5ed670d80b9d62a43
Taher <ttaherhagui@gmail.com>
2021-11-28 19:39:14 +0100

    update find_faces to detect face from webcam

--- FACE-RECOGNITION/find_faces.py
+++ FACE-RECOGNITION/find_faces.py
@@ -1,28 +1,47 @@
 import sys 
 import dlib
 from skimage import io 
-#Take the image file name from the command line 
-image = sys.argv[1]
+import cv2 as cv 
 
-# Create a HOG face detector using the built-in dlib class 
-face_detector = dlib.get_frontal_face_detector()
+def detect_and_display_faces_with_dlib():
+  #Take the image file name from the command line 
+  image = sys.argv[1]
 
-win = dlib.image_window()
+  # Create a HOG face detector using the built-in dlib class 
+  face_detector = dlib.get_frontal_face_detector()
 
-# load the image into an array 
-img = io.imread(image)
+  win = dlib.image_window()
 
-#Run the HOG face detector on the image data.
-# The result will be the bounding boxes of the faces in our images.
-detected_faces = face_detector(img, 1)
-print(f"I FOUND {len(detected_faces)} faces in the file {image}")
+  # load the image into an array 
+  img = io.imread(image)
 
-#Open a window on the desktop showing the image .
-win.set_image(img)
-# Loop through each face we found in the image.
-for i, face in enumerate(detected_faces):
-  win.add_overlay(face)
+  #Run the HOG face detector on the image data.
+  # The result will be the bounding boxes of the faces in our images.
+  detected_faces = face_detector(img, 1)
+  print(f"I FOUND {len(detected_faces)} faces in the file {image}")
 
-# wait until the user hit <enter> to close the window.
-dlib.hit_enter_to_continue()
+  #Open a window on the desktop showing the image .
+  win.set_image(img)
+  # Loop through each face we found in the image.
+  for i, face in enumerate(detected_faces):
+    win.add_overlay(face)
+
+  # wait until the user hit <enter> to close the window.
+  dlib.hit_enter_to_continue()
+
+def detect_faces():
+    cap = cv.VideoCapture(0)
+    face_detector = dlib.get_frontal_face_detector()
+    while(True):
+      _,frame = cap.read()
+      detected_faces = face_detector(frame)
+      for i, face in enumerate(detected_faces) :
+        x1,y1,x2,y2 = face.left(), face.top(), face.right(), face.bottom()
+        cv.rectangle(frame,(x1, y1),(x2, y2), (255,255,0), 2)
+      cv.imshow("detected",frame)
+      key = cv.waitKey(1) & 0xFF
+      if key == ord('q'):
+        break
+if __name__ == "__main__":
+    detect_faces()
 
